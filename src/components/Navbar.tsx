b446042e87542142238e828b8f7a9a709bd9e760
Anjana Kavindu <anjanakavinducampus@gmail.com>
2024-05-28 15:55:26 +0530

    css changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,9 @@ export class Navbar extends React.Component<{user: User | undefined}> {
     render() {
         let loginLogout: any;
         if (this.props.user) {
-            loginLogout = <Link to='/logout'>{this.props.user.userName}</Link>
+            loginLogout = <Link to='/logout' style={{float:"right"}}>{this.props.user.userName}</Link>
         } else {
-            loginLogout = <Link to='/login'>Login</Link>
+            loginLogout = <Link to='/login' style={{float:"right"}}>Login</Link>
         }
 
         return (
