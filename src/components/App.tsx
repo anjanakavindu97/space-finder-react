8ce8b5f19dff97341c75c207bab43cf6e6ed520d
Anjana Kavindu <anjanakavinducampus@gmail.com>
2024-05-28 15:38:57 +0530

    Navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { User } from '../models/Model'
 import { AuthService } from '../services/AuthService'
 import { Loging } from './Login';
+import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar } from './Navbar';
+import { Home } from './Home';
+import { Profile } from './Profile';
 
 interface AppState{
   user: User | undefined
@@ -13,6 +17,9 @@ export class App extends React.Component<{}, AppState>{
 
   constructor(props: any) {
     super(props)
+    this.state = {
+      user: undefined
+    }
 
     this.setUser = this.setUser.bind(this)
   }
@@ -26,9 +33,17 @@ export class App extends React.Component<{}, AppState>{
 
   render() {
     return (
-      <div>
-        App from class work....
-        <Loging authService={this.authService} setUser={this.setUser}/>
+      <div className='wrapper'>
+        <Router>
+          <div>
+            <Navbar user={this.state?.user}/>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/login' element={<Loging authService={this.authService} setUser={this.setUser}/>} />
+              <Route path='/profile' element={<Profile/>} />
+            </Routes>
+          </div>
+        </Router>
       </div>
     );
   }
